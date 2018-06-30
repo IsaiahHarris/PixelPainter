31a49d276ac7515daa58946c39ea73a4edde7d4e
Isaiah Harris <harrisfambam@gmail.com>
2018-06-30 13:38:50 -1000

    update

--- js/PixelPainter.js
+++ js/PixelPainter.js
@@ -1,16 +1,26 @@
+
 console.log('sanityCheck');
 
 let pixelPaintDiv = document.getElementById("pixelPainter")
+
+
 let gridSize = 12;
     for (let i = 0; i < gridSize; i++) {
         for (let j = 0; j < gridSize; j++) {
-            pixelPaintDiv.innerHTML += '<div class = "cels"> </div>';
+            var c=document.createElement("div");
+            pixelPaintDiv.appendChild(c);
+            c.className="cels";
+            c.innerHTML = 'hi';
         }
         pixelPaintDiv.innerHTML += '\n';
     }
 
-    let cel = document.getElementById('cels');
-    function changeCellColor(){
-        console.log('hi');
+    let pix = document.getElementsByClassName('cels')
+
+    for (let i = 0; i < pix.length; i++) {
+        pix[i].addEventListener('click', function(){
+            pix[i].style.color = 'green';
+        })
+
     }
-    cel.addEventListener('click', changeCellColor);
+    
